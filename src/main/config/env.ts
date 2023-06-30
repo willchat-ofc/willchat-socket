6ad2f5c24ec04164dce5e54ca9b8bb3af37fd4ad
Willian Cavalcanti Coelho <willian.cavalcanti@jala.university>
2023-06-30 19:21:45 -0300

    deploy settings

---
+++ src/main/config/env.ts
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-magic-numbers */
+import { config } from "dotenv";
+
+config();
+
+export const env = {
+  chatApi: process.env.CHAT_API || "http://localhost:3000",
+  port: process.env.PORT || 8080,
+};
--- src/main/server.ts
+++ src/main/server.ts
@@ -1,10 +1,7 @@
 import { server } from "./config/app";
 import { logger } from "../utils/logger";
+import { env } from "./config/env";
 
-const portLocal = 8080;
-const portHost = process.env.PORT;
-const PORT = portHost || portLocal;
-
-server.listen(PORT, () => {
-  logger.info("listening on port " + PORT);
+server.listen(env.port, () => {
+  logger.info("listening on port " + env.port);
 });
