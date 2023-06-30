92e92174a0e4e375c7b1e62f3b178cf409b056fd
Willian Cavalcanti Coelho <willian.cavalcanti@jala.university>
2023-06-30 14:59:24 -0300

    test: ensure receive-all-messages returns correct values and calls api.get with correct value

---
+++ tests/data/usecase/receive-all-messages.spec.ts
@@ -0,0 +1,48 @@
+import { DbReceiveAllMessages } from "../../../src/data/usecase/receive-all-messages";
+import type { ReceiveAllMessagesInput } from "../../../src/domain/usecase/receive-all-messages";
+import { api } from "../../../src/main/config/axios";
+
+jest.mock("../../../src/main/config/axios");
+
+const makeSut = () => {
+  const sut = new DbReceiveAllMessages();
+
+  return {
+    sut,
+  };
+};
+
+const fakeData: ReceiveAllMessagesInput = {
+  key: "fake-key",
+};
+
+const fakeResponse = {
+  data: [
+    {
+      id: "fake-id",
+      message: "fake-message",
+      userId: "fake-user-id",
+      userName: "fake-user-name",
+    },
+  ],
+};
+
+describe("ReceiveAllMessages Database", () => {
+  jest.spyOn(api, "get").mockResolvedValue(fakeResponse);
+
+  test("should call api with correct values", async () => {
+    const { sut } = makeSut();
+
+    await sut.get(fakeData);
+
+    expect(api.get).toBeCalledWith(`/key/message/${fakeData.key}`);
+  });
+
+  test("should returns if success a list of messages", async () => {
+    const { sut } = makeSut();
+
+    const res = await sut.get(fakeData);
+
+    expect(res).toStrictEqual(fakeResponse.data);
+  });
+});
