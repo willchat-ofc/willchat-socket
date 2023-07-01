2ca13b867f72a6bfbbaf82eccecd501eebc38a44
Willian Cavalcanti Coelho <willian.cavalcanti@jala.university>
2023-07-01 20:59:51 -0300

    test: implements validation on send-message factory layer

---
+++ tests/main/factory/send-message.spec.ts
@@ -0,0 +1,19 @@
+import { makeSendMessageValidation } from "../../../src/main/factory/send-message/send-message-validator";
+import { RequiredFieldValidation } from "../../../src/presentation/helpers/validators/required-field-validation/required-field-validation";
+import { ValidationComposite } from "../../../src/presentation/helpers/validators/validation-composite";
+import type { Validation } from "../../../src/presentation/protocols/validation";
+
+jest.mock("../../../src/presentation/helpers/validators/validation-composite");
+
+describe("DeleteKey Validation", () => {
+  test("should call Validation composite with valid Validations", () => {
+    makeSendMessageValidation();
+    const validations: Array<Validation> = [];
+
+    for (const field of ["key", "message", "userName", "userId"]) {
+      validations.push(new RequiredFieldValidation(field));
+    }
+
+    expect(ValidationComposite).toBeCalledWith(validations);
+  });
+});
