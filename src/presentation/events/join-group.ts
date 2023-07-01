76be16eb284b0b30a2582831fbdcb0b68278535d
Willian Cavalcanti Coelho <willian.cavalcanti@jala.university>
2023-07-01 18:58:00 -0300

    test: ensure JoinGroupEvent emites an Error when socket.join throws

--- src/presentation/events/join-group.ts
+++ src/presentation/events/join-group.ts
@@ -1,13 +1,12 @@
 import type { Socket } from "socket.io";
 import type { Event } from "../protocols/event";
-import { logger } from "../../utils/logger";
 
 export class JoinGroupEvent implements Event {
   public async handle(socket: Socket, data: any): Promise<void> {
     try {
       socket.join(data.key);
     } catch (err) {
-      logger.fatal(err);
+      socket.emit("Error", err);
     }
   }
 }
--- tests/presentation/events/join-group.spec.ts
+++ tests/presentation/events/join-group.spec.ts
@@ -16,9 +16,21 @@ const fakeData = {
 describe("JoinGroup Event", () => {
   test("should call socket.join with correct values", async () => {
     const { sut } = makeSut();
-
     await sut.handle(socketMock, fakeData);
 
     expect(socketMock.join).toBeCalledWith(fakeData.key);
+  });
+
+  test("should emit an error when throws", async () => {
+    const { sut } = makeSut();
+
+    const err = new Error();
+    jest.spyOn(socketMock, "join").mockImplementation(() => {
+      throw err;
+    });
+
+    await sut.handle(socketMock, fakeData);
+
+    expect(socketMock.emit).toBeCalledWith("Error", err);
   });
 });
