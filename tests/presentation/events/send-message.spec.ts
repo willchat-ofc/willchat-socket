1e669a26a94bb282b55a5190df26e7eccd6a4259
Willian Cavalcanti Coelho <willian.cavalcanti@jala.university>
2023-07-01 19:15:25 -0300

    test: ensure SendMessageEvent emites an error when sendMessage throws

--- tests/presentation/events/send-message.spec.ts
+++ tests/presentation/events/send-message.spec.ts
@@ -46,4 +46,15 @@ describe("SendMessage Event", () => {
 
     expect(sendMessage.send).toBeCalledWith(fakeData);
   });
+
+  test("should emit an error when throws", async () => {
+    const { sut, sendMessage } = makeSut();
+
+    const err = new Error();
+    jest.spyOn(sendMessage, "send").mockRejectedValue(new Error());
+
+    await sut.handle(socketMock, fakeData);
+
+    expect(socketMock.emit).toBeCalledWith("Error", err);
+  });
 });
