2f140707cd3c9ee7144ec1f9376bb0dd32fe8d9f
Willian Cavalcanti Coelho <willian.cavalcanti@jala.university>
2023-08-17 23:39:27 -0300

    chore: set up https server

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,11 +1,26 @@
 import express from "express";
 import http from "http";
+import https from "https";
 import { Server } from "socket.io";
 import { makeEvents } from "./events";
+import { env } from "./env";
 
 const app = express();
 
-const server = http.createServer(app);
+let server: any;
+
+if (env.cert) {
+  server = https.createServer(
+    {
+      cert: env.cert,
+      key: env.key,
+    },
+    app
+  );
+} else {
+  server = http.createServer(app);
+}
+
 const io = new Server(server);
 makeEvents(io);
 
--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -6,4 +6,6 @@ config();
 export const env = {
   chatApi: process.env.CHAT_API || "http://localhost:3000",
   port: process.env.PORT || 8080,
+  key: process.env.KEY,
+  cert: process.env.CERT,
 };
