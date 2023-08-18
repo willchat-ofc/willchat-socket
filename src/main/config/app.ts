7cbe82aed214732c8ee915a13da78524c84c1c83
Willian Cavalcanti Coelho <willian.cavalcanti@jala.university>
2023-08-18 01:15:24 -0300

    fix

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -3,16 +3,24 @@ import https from "https";
 import { Server } from "socket.io";
 import { makeEvents } from "./events";
 import { env } from "./env";
+import http from "http";
+import fs from "fs";
 
 const app = express();
 
-const server = https.createServer(
-  {
-    cert: env.cert,
-    key: env.key,
-  },
-  app
-);
+let server: any;
+
+if (env.cert) {
+  server = https.createServer(
+    {
+      cert: fs.readFileSync(env.cert),
+      key: fs.readFileSync(env.key),
+    },
+    app
+  );
+} else {
+  server = http.createServer(app);
+}
 
 const io = new Server(server);
 makeEvents(io);
