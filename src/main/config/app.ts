cdafef1f01bd45df21a0c64e4d8f04f69f61178b
Willian Cavalcanti Coelho <willian.cavalcanti@jala.university>
2023-08-18 00:55:48 -0300

    fix

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,5 +1,4 @@
 import express from "express";
-import http from "http";
 import https from "https";
 import { Server } from "socket.io";
 import { makeEvents } from "./events";
@@ -7,19 +6,13 @@ import { env } from "./env";
 
 const app = express();
 
-let server: any;
-
-if (env.cert) {
-  server = https.createServer(
-    {
-      cert: env.cert,
-      key: env.key,
-    },
-    app
-  );
-} else {
-  server = http.createServer(app);
-}
+const server = https.createServer(
+  {
+    cert: env.cert,
+    key: env.key,
+  },
+  app
+);
 
 const io = new Server(server);
 makeEvents(io);
