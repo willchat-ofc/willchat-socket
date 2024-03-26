1223bd97fef9d050d91198e30b49494b23441199
willian <willian.cacoelho@gmail.com>
2024-03-25 22:52:44 -0300

    chore: set up docker-compose and update all dependencies

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -4,7 +4,7 @@ import { config } from "dotenv";
 config();
 
 export const env = {
-  chatApi: process.env.CHAT_API || "http://localhost:3000",
+  chatApi: process.env.CHAT_API,
   port: process.env.PORT || 8080,
   key: process.env.KEY,
   cert: process.env.CERT,
